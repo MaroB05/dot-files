35d712097ee2aa2aa35a9ba230b54d16d05bda59
MaroB05 <s-marwan.hussien@zewailcity.edu.eg>
2025-03-08 16:17:27 +0200

    updates

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -4,7 +4,8 @@ alias reboot="sudo reboot"
 alias blue="sudo bluetoothctl"
 alias ls="eza --icons"
 alias nvim="$HOME/i3-dots/scripts/run_nvim.sh"
-
+alias venv="python3 -m venv"
+alias activate="source env/bin/activate"
 
 new_course(){
   mkdir $1
@@ -14,4 +15,15 @@ new_course(){
   mkdir problems solutions
   cd ../../
   echo "Happy suffering!"
+}
+
+createproject(){
+  mkdir $1
+  cd $1
+  echo "created" $1 "...."
+  venv env
+  echo "virtual env is ready!"
+  mkdir src/
+  touch src/main.py
+  echo "happy pythoning..!"
 }
